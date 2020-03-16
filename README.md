09ce803f4e5c6dc8c07dad0f799e53a8f5ab6008
Alejandro Saenz <alejandro.saenz@softrams.com>
2020-03-16 11:55:11 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Costco Buddy
+
+A Costco stock notification CLI tool.
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+### Prerequisites
+
+Make sure you have [Node.js](http://nodejs.org/).
+
+### Installing
+
+```
+npm install
+npm start
+```
+
+Create a `.env` file with the following properties:
+```
+FROM_EMAIL=
+FROM_EMAIL_PASSWORD=
+TO_EMAIL=
+```
+## License
+This project is licensed under the MIT License - see the [LICENSE.md](https://github.com/Whamo12/idor-chat/blob/master/LICENSE) file for details
---
+++ src/app.js
@@ -0,0 +1,95 @@
+const axios = require('axios').default;
+const readline = require('readline');
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+let transporter = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    user: process.env.FROM_EMAIL,
+    pass: process.env.FROM_EMAIL_PASSWORD
+  }
+});
+let urlAry = [];
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: 'Welcome to Costo Buddy! Please enter a comma delimited list of Costo product URLs>'
+});
+rl.prompt();
+rl.on('line', line => {
+  urlAry = line.split(',');
+  if (urlAry.length) {
+    fetchItemStatus(urlAry);
+    setTimeout(() => {
+      fetchItemStatus(urlAry);
+    }, 1000 * 60 * 60);
+  } else {
+    console.info('Product list is empty.  Costco Buddy will now exit.');
+    process.exit();
+  }
+});
+
+/**
+ * @description Fetch status of target items
+ * @param {object} mailOptions
+ * @returns HTTP status code
+ */
+function fetchItemStatus(line) {
+  console.log('Fetching item status...');
+  urlAry.forEach(async (url, index, object) => {
+    try {
+      await axios.get(url, {
+        validateStatus: status => {
+          if (status === 200) {
+            console.info(`The following item is in stock: ${url}.  Please check your email!`);
+            sentInStockEmail(url);
+            object.splice(index, 1);
+            if (!object.length) {
+              console.info('All items in the product list are now in stock.  Costco Buddy will now exit.');
+              process.exit();
+            }
+            return;
+          } else {
+            console.info(
+              `The following item is still out of stock: ${url}.  Costco Buddy will check again in 1 (one) hour!`
+            );
+            return;
+          }
+        }
+      });
+    } catch (error) {
+      // TODO: Figure out why Axios is throwing an exception even if the status is 200
+    }
+  });
+}
+
+/**
+ * @description Send email
+ * @param {object} mailOptions
+ * @returns string
+ */
+function sendEmail(mailOptions) {
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return 'Error sending email';
+    } else {
+      return 'email sent successfully';
+    }
+  });
+}
+
+/**
+ * @description Prepare account verification email
+ * @param {string} uuid
+ * @returns string
+ */
+function sentInStockEmail(url) {
+  const mailOptions = {
+    from: process.env.FROM_EMAIL,
+    to: process.env.TO_EMAIL,
+    subject: 'Your Item is in Stock!',
+    text: `An Item you are watching is in stock! \n${url}`
+  };
+  sendEmail(mailOptions);
+}
